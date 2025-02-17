234bc4239cb542b0baeeed3b4eb9941e68302676
tls427wodnr <tlswodnr427@naver.com>
2025-02-17 16:10:58 +0900

    refactoring tableview

---
+++ BookReportApp0217/BookListView.swift
@@ -0,0 +1,58 @@
+//
+//  BookListView.swift
+//  BookReportApp0217
+//
+//  Created by tlswo on 2/17/25.
+//
+
+import UIKit
+
+class BookListView: UIView {
+
+    private var books: [BookItem] = []
+    private let tableView = UITableView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureTableView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureTableView() {
+        addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+        tableView.backgroundColor = .systemGray6
+    }
+    
+    func updateBooks(_ books: [BookItem]) {
+        self.books = books
+        tableView.reloadData()
+    }
+}
+
+extension BookListView: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = books[indexPath.row].title
+        return cell
+    }
+}
+
--- BookReportApp0217/ViewController.swift
+++ BookReportApp0217/ViewController.swift
@@ -10,29 +10,24 @@ import UIKit
 class ViewController: UIViewController {
     
     private var myBooks: [BookItem] = []
-    private let tableView = UITableView()
+    private let bookListView = BookListView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
-        configureTableView()
-        fetchBooks(query: "과학")
+        setupBookListView()
+        fetchBooks(query: "코딩")
     }
     
-    func configureTableView() {
-        view.addSubview(tableView)
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        
-        tableView.translatesAutoresizingMaskIntoConstraints = false
+    private func setupBookListView() {
+        view.addSubview(bookListView)
+        bookListView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            bookListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            bookListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bookListView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bookListView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
-        tableView.backgroundColor = .systemGray6
     }
     
     private func fetchBooks(query: String) {
@@ -41,21 +36,9 @@ class ViewController: UIViewController {
             
             DispatchQueue.main.async {
                 self.myBooks = books
-                self.tableView.reloadData()
+                self.bookListView.updateBooks(books)
             }
         }
     }
 }
 
-extension ViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return myBooks.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = myBooks[indexPath.row].title
-        return cell
-    }
-}
